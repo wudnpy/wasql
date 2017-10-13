4e9679e0b66e93aa6a89074bc9280bdbd8f6e61f
wudnpy <6326236python@gmail.com>
2017-10-13 06:03:01 +0300

    validators

--- validformapp/forms.py
+++ validformapp/forms.py
@@ -1,6 +1,12 @@
 from django import forms
+from django.core import validators
+
+def check_for_a(value):
+    if value[0].lower != 'a':
+        raise forms.ValidationError('Make needs to start with - a')
 
 class NameForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
-    text = forms.CharField(widget=forms.Textarea)
+    text = forms.CharField(widget=forms.Textarea,
+                            validators=[validators.MaxLengthValidator(5)])
