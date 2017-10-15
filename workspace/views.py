c35477b582f2a806f67c368b0bf7be8dd41cd421
wudnpy <6326236python@gmail.com>
2017-10-15 21:54:54 +0300

    15.10.2017 12:54
    Все збс все получилось!

--- workspace/views.py
+++ workspace/views.py
@@ -11,40 +11,26 @@ def news(request):
     })
 
 def queries(request):
-    #objects = Query.objects.all()
-
-    objects = Query.objects.all().values()
-
-    return render(request, 'workspace/queries.html', {'objects':objects})
-
-def query_detail(request, q_id):
-
-    objects = Query.objects.all().values()
-
     is_queries = True
 
-    query_form = QueryFormType0()
+    objects = Query.objects.all().values()
 
-    elements = {'query_form': query_form, 'is_queries': is_queries, }
+    return render(request, 'workspace/queries.html', {'objects':objects, 'is_queries':is_queries})
 
-    for item in request:
-        print(item)
+def query_detail(request, q_id):
+    is_queries = True # переменная служит для выделения раздела в котором мы находимся на сайте.
+
+    elements = { 'is_queries': is_queries }
+
+    q_object = Query.objects.filter(number=q_id).values()
+
+    elements.update(q_object[0])
+
     if request.method == 'POST':
-        query_form = QueryFormType0(request.POST)
 
-        if query_form.is_valid():
-            q_name = query_form.cleaned_data['name']
-            elements.update({'q_name': q_name})
-
-
-            # Обработка значения выбранного пользователем
-            for object in objects:
-                if str(object['name']) == str(q_name):
-                    q_number = object['number']
-                    elements.update({'q_number': q_number})
-                    q_total_time = object['total_time']
-                    elements.update({'q_total_time': q_total_time})
-            return redirect('/scauthapp')
+        q = request.POST
+        print(q.get('name'))
+        return redirect('/scauthapp')
     return render(request, 'workspace/query-detail.html', elements)
 
 def rating(request):
